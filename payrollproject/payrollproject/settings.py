4ffdab6f256908b2961e003ce0502ed268a22c89
aswanth2002-web <aswaa2918@gmail.com>
2026-03-03 18:28:38 +0530

    added

--- payrollproject/payrollproject/settings.py
+++ payrollproject/payrollproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xkm=hyk_8xfl0qb(e8z+4q$8_s^twr7%-usw7#-kmsj9smwor+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
