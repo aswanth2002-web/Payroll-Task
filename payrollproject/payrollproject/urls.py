b533f2417a9b82f7d72dd92c040cea8876cff82f
aswanth2002-web <aswaa2918@gmail.com>
2026-03-03 17:53:05 +0530

    updated

--- payrollproject/payrollproject/urls.py
+++ payrollproject/payrollproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/payrollapp/')),
     path('admin/', admin.site.urls),
     path('payrollapp/',include('payrollapp.urls'))
 ]
